d012eeac50c2195251460249842231c8703f7e99
ksushant881 <b180062@nitsikkim.ac.in>
2021-04-19 22:07:56 +0530

    Updated on 19?04/2021

--- codeforces/and_0_sum_big.cpp
+++ codeforces/and_0_sum_big.cpp
@@ -8,6 +8,13 @@ ios_base::sync_with_stdio(0);
 cin.tie(0);
 cout.tie(0);
  
-vector<int>temp;
-cout<<temp.size();
+int tc;
+cin >> tc;
+for(int i=0;i<tc;i++){
+    ll n,k;
+    cin >> n >> k;
+    ll limit = pow(2,k) - 1;
+    vector<ll>v(n);
+    
+}
 }
--- codeforces/table_decorations.cpp
+++ codeforces/table_decorations.cpp
@@ -8,6 +8,11 @@ ios_base::sync_with_stdio(0);
 cin.tie(0);
 cout.tie(0);
  
-vector<int>temp;
-cout<<temp.size();
+vector<ll>v(3);
+for(int i=0;i<3;i++){
+    cin >> v[i];
+}
+sort(v.begin(),v.end());
+cout<<min((v[0]+v[1]+v[2])/3,v[0]+v[1])<<endl;
+
 }
