bed233987fb3d43fea836fab7800e6ab815957fe
ksushant881 <b180062@nitsikkim.ac.in>
2021-04-20 07:47:44 +0530

    Started BitMasking

---
+++ BITS/check_kth_bit_is_set_or_not.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pi 3.1415926536
+#define ll long long int
+#define endl "\n"
+
+void isSet(int n,int k){
+    if(n & (1<<(k-1)) == 0)
+        cout << "NO" << endl;
+    else
+        cout << "YES" << endl;
+}
+
+int main(){
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+ 
+
+}
