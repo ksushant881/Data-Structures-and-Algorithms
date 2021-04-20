a391da999894eae006a9b4ec16d06d5ed66c8c7d
ksushant881 <b180062@nitsikkim.ac.in>
2021-04-20 07:34:38 +0530

    Update product_1_modulo_n.cpp

--- codeforces/product_1_modulo_n.cpp
+++ codeforces/product_1_modulo_n.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 #define pi 3.1415926536
 #define ll long long int
-
+#define mod 1000000007
+int size=0;
 void sub(vector<ll>v,int n,int inx,vector<vector<ll>>&res,vector<ll>temp,ll pro){
     if(inx == n){
-        if(pro%n == 1){
+        if(pro%n == 1 && temp.size() > size){
             res.push_back(temp);
+            size = temp.size();
         }
         return;
     }
     sub(v,n,inx+1,res,temp,pro);
     temp.push_back(v[inx]);
     pro*=v[inx];
+    pro = pro%mod;
     sub(v,n,inx+1,res,temp,pro);
 }
 
@@ -48,5 +51,4 @@ vector<vector<ll>>res;
 int inx = 0;
 sub(nums,n,inx,res,temp,1);
 findMax(res,n);
-
 }
