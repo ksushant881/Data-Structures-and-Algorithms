de5668a7505dd3273a1dd151d37b1dfe50303f27
ksushant881 <b180062@nitsikkim.ac.in>
2021-05-18 23:55:32 +0530

    Update RangeSumQuery2D.cpp

--- Leetcode/RangeSumQuery2D.cpp
+++ Leetcode/RangeSumQuery2D.cpp
@@ -20,26 +20,26 @@ public:
     }
 };
 
-{
+namespace solution{
     //Efficient solution
     class NumMatrix {
     public:
-    vector<vector<int>>arr;
-    int row,col;
-    vector<vector<int>>sum(row,vector<int>(col,0));
-    NumMatrix(vector<vector<int>>& matrix) {
-        arr=matrix;
-    }
-    
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=0;
-        for(int i=row1;i<=row2;i++){
-            for(int j=col1;j<=col2;j++){
-                sum+=arr[i][j];
-            }
+        vector<vector<int>>arr;
+        int row,col;
+        vector<vector<int>>sum(row,vector<int>(col,0));
+        NumMatrix(vector<vector<int>>& matrix) {
+            arr=matrix;
         }
-        return sum;
-    }
+        
+        int sumRegion(int row1, int col1, int row2, int col2) {
+            int sum=0;
+            for(int i=row1;i<=row2;i++){
+                for(int j=col1;j<=col2;j++){
+                    sum+=arr[i][j];
+                }
+            }
+            return sum;
+        }
 };
 
 }
