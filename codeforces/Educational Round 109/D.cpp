d5004beecd9d72f7b40cb492e513a88109def053
ksushant881 <b180062@nitsikkim.ac.in>
2021-05-19 23:02:13 +0530

    Upsolving

--- codeforces/Educational Round 109/D.cpp
+++ codeforces/Educational Round 109/D.cpp
@@ -4,21 +4,7 @@ using namespace std;
 #define ll long long int
 #define mod 1000000007
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
- 
-int dfs(vector<int>&v,int n,int curr,int i){
-    if(v[i]==0){
-        v[i]=1;
-        return curr-i;
-    }
-    int left,right;
-    
-    if(i!=0){
-        dfs(v,n,curr,i-1);
-    }
-    if(i!=n-1){
-        dfs(v,n,curr,i+1);
-    }
-}
+
 
 int main(){
 fastio
@@ -30,11 +16,41 @@ for(int i=0;i<n;i++){
     cin>>v[i];
 }
 
+int cost=0;
 for(int i=0;i<n;i++){
     if(v[i]==1){
         v[i]=2;
-        dfs(v,v.size(),i,i);
+        int left,right;
+        left=-1;
+        right=-1;
+        for(int j=i;j<n;j++){
+            if(v[j]==0){
+                right=j;
+                break;
+            }
+        }
+        for(int j=i;j>=0;j--){
+            if(v[j]==0){
+                left=j;
+                break;
+            }
+        }
+        
+        if(left!=-1 && right!=-1){
+            int res;
+            res=(abs(right-i) >= abs(left-i))?left:right;
+            v[res]=3;
+            cost+=(abs(i-res));
+        }
+        else if(left==-1 && right!=-1){
+            v[right]=3;
+            cost+=(abs(i-right));
+        }
+        else if(right==-1 && left!=-1){
+            v[left]=3;
+            cost+=(abs(i-left));
+        }
     }
 }
-
+cout<<cost<<"\n";
 }
