75e076517a58d47ccc85994bd7661232eca79b57
ksushant881 <b180062@nitsikkim.ac.in>
2021-05-24 00:53:37 +0530

    Updated on 24/05/2021

---
+++ Kickstart/Kickstart 2021/Round C/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pi 3.1415926536
+#define ll long long int
+#define mod 1000000007
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+int main(){
+fastio
+ 
+int tc=1;
+cin >> tc;
+for(int casess=0;casess<tc;casess++){
+ll n,k;
+cin>>n>>k;
+string s;
+cin>>s;
+ll res;
+
+
+cout<<"Case #"<<tc<<": "<<res%mod<<"\n";
+ 
+ 
+}
+}
