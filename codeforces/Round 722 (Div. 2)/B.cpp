eaba83975646c15a89e62e914162c479ec601ba1
ksushant881 <b180062@nitsikkim.ac.in>
2021-05-25 21:42:48 +0530

    Attended Round 722

--- codeforces/Round 722 (Div. 2)/B.cpp
+++ codeforces/Round 722 (Div. 2)/B.cpp
@@ -11,67 +11,35 @@ fastio
 int tc=1;
 cin >> tc;
 for(int casess=0;casess<tc;casess++){
-
- ll n;
- cin>>n;
- vector<ll>v(n);
- for(ll i=0;i<n;i++) cin>>v[i];
-vector<ll>pos;
-int lastPos=-1;
+ll n;
+cin>>n;
+vector<ll>v(n);
 vector<ll>neg;
-int zer=0;
-ll minNeg=1;
-ll maxNeg=INT32_MIN;
-for(int i=0;i<n;i++){
-    if(v[i]==0){
-        zer++;
-    }
-    if(v[i]<0){
-        neg.push_back(v[i]);
-        minNeg = min(v[i],minNeg);
-        maxNeg = max(v[i],maxNeg);
-    }
-    if(v[i]>0){
-        pos.push_back(v[i]);
-    }
+ll minPos=INT32_MAX;
+for(ll i=0;i<n;i++){
+    cin>>v[i];
+    if(v[i]<=0) neg.push_back(v[i]);
+    if(v[i]>0) minPos=min(minPos,v[i]);
 }
-ll res;
-ll diff=INT32_MAX;
+if(neg.size()==0){cout<<1<<endl; continue;}
 sort(neg.begin(),neg.end());
-if(neg.size()>2){
-for(ll i=0;i<neg.size()-1;i++){
+ll diff=INT32_MAX;
+if(neg.size()>1){
+for(int i=0;i<neg.size()-1;i++){
     diff=min(diff,abs(neg[i+1]-neg[i]));
 }
 }
-// for(ll i=0;i<neg.size();i++){
-//     for(ll j=0;j<neg.size();j++){
-//         if(i==j) continue;
-//         diff=min(diff,abs(neg[i]-neg[j]));
-//     }
-// }
+if(neg.size()==1) diff=neg[0];
 
-if(pos.size()==n){
-    cout<<1<<"\n";
-    continue;
+ll ans=neg.size();
+
+if(diff==INT32_MAX){
+    cout<<ans<<endl;
 }
 
-if(zer==0){
-    res=neg.size();
+if(abs(diff) >= minPos){
+    ans++;
 }
-else{
-    if(pos.size()!=0)
-        res=neg.size()+1;
-    else
-        res=neg.size()+zer;
-}
-
-for(int i=0;i<pos.size();i++){
-    if(pos[i]<=diff){
-        res++;
-        break;
-    }
-}
-cout<<res<<"\n";
-
+cout<<ans<<endl;
 }
 }
