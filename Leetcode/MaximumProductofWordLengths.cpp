668df3b3476f14e29ead7b5c8009aa981e2bb54b
ksushant881 <b180062@nitsikkim.ac.in>
2021-05-27 21:38:23 +0530

    Leetcode daily challenge

---
+++ Leetcode/MaximumProductofWordLengths.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution2 {
+public:
+    int maxProduct(vector<string>& words) {
+        int n=words.size();
+        vector<set<char>>v;
+
+        for(int i=0;i<n;i++){
+            set<char>st;
+            for(int j=0;j<words[i].length();j++){
+                st.insert(words[i][j]);
+            }
+            v.push_back(st);
+        }
+
+        int res=0;
+        for(int i=0;i<n;i++){
+            // cout<<"i = "<<i<<endl;
+            // for(auto x:v[i]) cout<<x<<" ";
+            // cout<<" "<<endl;
+            for(int j=0;j<n;j++){
+                // for(auto x:v[j]) cout<<x<<" ";
+                // cout<<" "<<endl;
+                bool flag=true;
+                for(auto x:v[i]){
+                    if(v[j].count(x)){
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag){
+                    int temp=words[i].size()*words[j].size();
+                    res=max(res,temp);
+                }
+            }
+        }
+        return res;
+    }
+    
+};
+
+class Solution{
+    public:
+        int maxProduct(vector<string>& words){
+            int n=words.size();
+            int res=0;
+            vector<int>mask(n);
+            for(int i=0;i<n;i++){
+                for(auto &ch:words[i]){
+                    mask[i] |= 1 << (ch-'a');
+                }
+                for(int j=0;j<i;j++){
+                    if((mask[i] & mask[j]) == 0){
+                        int temp=words[i].size()*words[j].size();
+                        res=max(res,temp);
+                    }
+                }
+            }
+            return res;
+        }
+};
+
+
+int main(){
+
+vector<string>words1={"abaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaacw","baz","foo","bar","xtfn","abcdef"};
+vector<string>words2={"a","ab","abc","d","cd","bcd","abcd"};
+vector<string>words3={"a","aa","aaa","aaaa"};
+Solution s;
+cout<<s.maxProduct(words1)<<endl;
+cout<<s.maxProduct(words2)<<endl<<s.maxProduct(words3)<<endl;
+
+}
--- Leetcode/tempCodeRunnerFile.cpp
+++ Leetcode/tempCodeRunnerFile.cpp
@@ -1,16 +1,2 @@
-   // if(res[i]==v){
-    //     if(i!=res.size()-1){
-    //     // for(int j=0;j<res[i+1].size();j++){
-    //     //     cout<<res[i+1][j]<<" ";
-    //     v=res[i+1];
-    //      }
 
-        
-    //     else{
-    //     // for(int j=0;j<res[0].size();j++){
-    //     //     cout<<res[0][j]<<" ";
-    //     // }
-    //     v=res[0];
-    //     }
-    //     break;
-    // }
+            return ans;
