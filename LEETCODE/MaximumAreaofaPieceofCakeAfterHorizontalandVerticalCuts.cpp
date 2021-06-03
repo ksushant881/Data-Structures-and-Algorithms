1ddac208e07e770726460254af19929123a19734
ksushant881 <b180062@nitsikkim.ac.in>
2021-06-03 22:49:18 +0530

    Update MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCuts.cpp

--- LEETCODE/MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCuts.cpp
+++ LEETCODE/MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCuts.cpp
@@ -1,20 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define mod 1000000007
+
 class Solution{
     public:
+    #define mod 1000000007
+    #define ll long long
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        horizontalCuts.push_back(0);
+        horizontalCuts.push_back(h);
+        verticalCuts.push_back(0);
+        verticalCuts.push_back(w);
         sort(horizontalCuts.begin(),horizontalCuts.end());
         sort(verticalCuts.begin(),verticalCuts.end());
-        int n1=horizontalCuts.size();
-        int n2=verticalCuts.size();
-        int res1=horizontalCuts[1]-horizontalCuts[0];
-        int res2=verticalCuts[0]-verticalCuts[0];
+        ll n1=horizontalCuts.size();
+        ll n2=verticalCuts.size();
+        ll res1=horizontalCuts[1]-horizontalCuts[0];
+        ll res2=verticalCuts[1]-verticalCuts[0];
         for(int i=1;i<n1;i++){
-            res1=max(res1,horizontalCuts[i]-horizontalCuts[i-1]);
+            res1=max(res1,(ll)horizontalCuts[i]-horizontalCuts[i-1]);
         }
         for(int i=1;i<n2;i++){
-            res2=max(res2,verticalCuts[i]-verticalCuts[i-1]);
+            res2=max(res2,(ll)verticalCuts[i]-verticalCuts[i-1]);
         }
         return (res1*res2)%mod;
     }
