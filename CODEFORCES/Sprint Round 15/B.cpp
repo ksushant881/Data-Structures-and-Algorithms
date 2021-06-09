1220b562dcc064afebab38b8fa0caae8f758de9d
ksushant881 <b180062@nitsikkim.ac.in>
2021-06-09 22:26:25 +0530

    Attended Sprint Round 15

---
+++ CODEFORCES/Sprint Round 15/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pi 3.1415926536
+#define ll long long int
+#define mod 1000000007
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main(){
+fastio
+ 
+string stupid = "I'm too stupid to solve this problem";
+ll n;
+cin>>n;
+
+// int arr[n];
+// arr[1]=1;
+// arr[2]=1;
+// arr[3]=2;
+// for(int i=4;i<n;i++){
+//     arr[i]=arr[i-1]+arr[i-2];
+// }
+
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
+//         for(int k=0;k<n;k++){
+//             if(arr[i]+arr[j]+arr[k] == n){
+//                 cout<<arr[i]<<" "<<arr[j]<<" "<<arr[k]<<endl;
+//                 return 0;
+//             }
+//         }
+//     }
+// }
+// cout<<stupid;
+cout<<0<<" "<<0<<" "<<n;
+
+}
