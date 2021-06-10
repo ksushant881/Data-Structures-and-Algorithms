808be896a202229c2dd8a1def175058c1995207d
ksushant881 <b180062@nitsikkim.ac.in>
2021-06-10 19:54:14 +0530

    Upsolving

--- CODEFORCES/Round 724 (DIV 2)/C.cpp
+++ CODEFORCES/Round 724 (DIV 2)/C.cpp
@@ -10,47 +10,23 @@ fastio
  
 int tc=1;
 cin >> tc;
-
 for(int casess=0;casess<tc;casess++){
 int n;
 cin>>n;
 string s;
 cin>>s;
-
-int d=0;
-int k=0;
-vector<vector<int>>v;
+map<pair<int,int>,int>mp;
+int d=0,k=0;
 for(int i=0;i<n;i++){
-    vector<int>temp;
     if(s[i]=='D') d++;
     else k++;
-    int t = __gcd(d,k);
-    temp={d,k,t};
-    v.push_back(temp);
-    
+    int g=__gcd(d,k);
+    int currd=d/g;
+    int currk=k/g;
+    mp[{currd,currk}]++;
+    cout<<mp[{currd,currk}]<<" ";
 }
-cout<<1<<" ";
-for(int i=1;i<n;i++){
-    int countd=v[i][0]/v[i][2];
-    int countk=v[i][1]/v[i][2];
-    int len = countd + countk;
-    int a=0,b=0;
-    bool flag=false;
-    for(int j=1;j<(i+1)/len;j++){
-        if(v[(len-1)*j][0]-a != countd && v[(len-1)*j][1]-b != countk){
-            flag=true;
-            break;
-        }
-        else{
-            a=v[(len-1)*j][0];
-            b=v[(len-1)*j][1];
-        }
-    }
-    if(!flag) cout<<v[i][2]<<" ";
-    else cout<<1<<" ";
-}
-cout<<endl;
-
-
+ cout<<endl;
+ 
 }
 }
