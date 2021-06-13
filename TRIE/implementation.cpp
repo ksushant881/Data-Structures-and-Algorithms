0edda5407b0abaf8f1f4c97dfe879b9348c42c14
ksushant881 <b180062@nitsikkim.ac.in>
2021-06-13 17:12:51 +0530

    Update implementation.cpp

--- TRIE/implementation.cpp
+++ TRIE/implementation.cpp
@@ -2,21 +2,60 @@
 using namespace std;
 
 struct trieNode{
-    trieNode *child[26];
+    trieNode *child[26]; //every node can have upto 26 branches
     bool isEnd;
+
     trieNode(){
         isEnd = false;
         for(int i=0;i<26;i++)
             child[i]=NULL;
     }
 
-    void insert(string s,int n){
-        for(int i=0;i<n;i++){
-            if(child[s[i]='a'] == NULL){
-                trieNode*newChild;
-                child[s[i]-'a'] = newChild;
-            }
+    void insert(string key,trieNode*root){
+        trieNode*curr = root;
+        for(int i=0;i<key.size();i++){
+            int index = key[i]-'a';
+            if(curr->child[index] == NULL)
+                curr->child[index]=new trieNode();
+            curr=curr->child[index];
         }
+        curr->isEnd = true;
+    }
+
+    bool search(string key,trieNode*root){
+        trieNode*curr = root;
+        for(int i=0;i<key.length();i++){
+            int index=key[i]='a';
+            if(curr->child[index] == NULL) return false;
+            curr=curr->child[index];
+        }
+        return curr->isEnd;
+    }   
+
+    //recursive function to delete key from dictionary
+    bool isEmpty(trieNode*root){
+        for(int i=0;i<26;i++){
+            if(root->child[i]!=NULL) return false;
+        }
+        return true;
+    }
+    trieNode* deleteWord(string key,trieNode*root,int inx){
+        if(root == NULL) return NULL;
+        if(inx == key.length()){
+            root->isEnd = false;
+            if(isEmpty(root)){
+                delete(root);
+                root=NULL;
+            }
+            return root;
+        }
+        int index = key[inx]-'a';
+        root->child[inx]=deleteWord(key,root->child[inx],inx+1);
+        if(isEmpty(root) && root->isEnd==false){
+            delete(root);
+            root=NULL;
+        }
+        return root;
     }
 };
 
