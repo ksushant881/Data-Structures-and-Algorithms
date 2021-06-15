37c48d680cfbcbe42fc38d031af80a4c87101ff8
ksushant881 <b180062@nitsikkim.ac.in>
2021-06-15 12:51:13 +0530

    Update BinaryTreePaths.cpp

--- LEETCODE/BinaryTreePaths.cpp
+++ LEETCODE/BinaryTreePaths.cpp
@@ -9,7 +9,7 @@ using namespace std;
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
-
+  
 class Solution {
 public:
     void help(TreeNode*root,vector<string>&v,string s){
