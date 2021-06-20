8f3609a0ec7f7c8b38c549e75986443cb7997504
Sushant <71615295+ksushant881@users.noreply.github.com>
2021-06-21 03:48:36 +0530

    Delete Atcoder directory

--- Atcoder/ARC 119/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-    ll res=INT64_MIN;
-    for(int i=0;i<60;i++){
-        ll a=n/(v[i]);
-        ll c=n-a*(v[i]);
-        ll b=i;
-        if(a*v[i]+c > n) break;
-        res=max(res,a+b+c);
-    }
-    cout<<res;
