c7f12c5a217a302dbc305707ee69eee635a1828f
ksushant881 <b180062@nitsikkim.ac.in>
2021-06-21 09:34:43 +0530

    Update C.cpp

--- CODEFORCES/Round 725 (Div. 3)/C.cpp
+++ CODEFORCES/Round 725 (Div. 3)/C.cpp
@@ -3,34 +3,60 @@ using namespace std;
 #define pi 3.1415926536
 #define ll long long int
 #define mod 1000000007
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
- 
-int main(){
-fastio
- 
-int tc=1;
-cin >> tc;
-for(int casess=0;casess<tc;casess++){
-        ll n,l,r;
-        cin>>n>>l>>r;
-        vector<ll>v(n);
-        for(int i=0;i<n;i++){
-            cin>>v[i];
-        }
-        sort(v.begin(),v.end());
-        ll ans=0;
-        for(ll i=0;i<n;i++){
-            ll a=lower_bound(v.begin(),v.end(),l-v[i])-v.begin()+1;
-            ll b=upper_bound(v.begin(),v.end(),r-v[i])-v.begin()+1;
-            if(b==n || v[b]+v[i]>r)
-                b--;
-            ans+=b-a+1;
-            if(a<=i && i<=b)
-                ans--;
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
+int main()
+{
+    fastio
+
+    int tc = 1;
+    cin >> tc;
+    for (int casess = 0; casess < tc; casess++)
+    {
+        ll n, lLimit, rLimit;
+        cin >> n >> lLimit >> rLimit;
+        vector<ll> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
         }
-        cout<<ans/2<<endl;
- 
- 
-}
+        sort(v.begin(), v.end());
+        ll ans = 0;
+        for (ll i = 0; i < n; i++)
+        {
+            int lFind = lLimit - v[i];
+            int rFind = rLimit - v[i];
+
+            if(v[i] + v[i+1] > rLimit) break;
+            
+            int high = n;
+            int low = i;
+            while(high - low > 1){
+                int mid = low + (high - low)/2;
+                if(v[mid] >= lFind){
+                    high = mid;
+                }
+                else{
+                    low = mid;
+                }
+            }
+            int leftPairs = high;
+
+            high = n;
+            low = i;
+            while(high - low > 1){
+                int mid = low + (high - low)/2;
+                if(v[mid] <= rFind){
+                    low = mid;
+                }
+                else{
+                    high = mid;
+                }
+            }
+            int rightPairs = low;
+
+            ans+=(rightPairs - leftPairs + 1);
+        }
+        cout << ans << endl;
+    }
 }
