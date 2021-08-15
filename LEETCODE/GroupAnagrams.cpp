f3a3b445302d1484d2170711e4eb329a7038c7a0
ksushant881 <ksushant881@gmail.com>
2021-08-15 14:18:01 +0530

    Create GroupAnagrams.cpp

---
+++ LEETCODE/GroupAnagrams.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        int n=strs.size();
+        vector<vector<int>>freq(n,vector<int>(26,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<strs[i].length();j++){
+                freq[i][strs[i][j]-'a']++;
+            }
+        }
+        vector<bool>added(n,false);
+        for(int i=0;i<n-1;i++){
+            vector<string>temp;
+            if(!added[i]){
+                temp.push_back(strs[i]);
+            }
+            else{
+                continue;
+            }
+            for(int j=i+1;j<n;j++){
+                if(!added[j]){
+                    if(freq[i]==freq[j]){
+                        temp.push_back(strs[j]);
+                    }
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
+int main(){
+
+}
