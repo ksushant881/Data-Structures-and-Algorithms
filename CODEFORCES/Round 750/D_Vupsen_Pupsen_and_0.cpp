023cd0fa6bf6a5be076172547ae9b986fca60d94
ksushant881 <ksushant881@gmail.com>
2021-10-25 20:29:43 +0530

    Update D_Vupsen_Pupsen_and_0.cpp

--- CODEFORCES/Round 750/D_Vupsen_Pupsen_and_0.cpp
+++ CODEFORCES/Round 750/D_Vupsen_Pupsen_and_0.cpp
@@ -34,10 +34,24 @@ else{ // n is odd, do same as even for first n-3 array elements
         cout<<ans[i]<<" ";
     }
     //arr[n-3] arr[n-2] arr[n-1]
-    int last_third = -arr[n-1];
-    int last_second = -arr[n-1];
-    int last = arr[n-3]+arr[n-2];
-    cout<<last_third<<" "<<last_second<<" "<<last<<endl;
+    int a,b,c;
+    if(arr[n-3]+arr[n-2]!=0){
+        c=arr[n-3]+arr[n-2];
+        a = -arr[n-1];
+        b= -arr[n-1];
+    }
+    else if(arr[n-3]+arr[n-1]!=0){
+        b=arr[n-3]+arr[n-1];
+        a=-arr[n-2];
+        c=-arr[n-2];
+    }
+    else if(arr[n-2]+arr[n-1]!=0){
+        a=arr[n-2]+arr[n-1];
+        b=-arr[n-3];
+        c=-arr[n-3];
+    }
+    
+    cout<<a<<" "<<b<<" "<<c<<endl;
 }
 }
 
