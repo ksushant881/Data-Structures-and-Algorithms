51f2986dcebcc6ef1a0978645b3e7a77d9d4b0b9
ksushant881 <ksushant881@gmail.com>
2021-10-25 20:29:08 +0530

    Create MinStack.cpp

---
+++ LEETCODE/MinStack.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MinStack {
+public:
+    stack<int>st1;
+    stack<int>st2;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st1.push(val);
+        if(st2.empty() || val<=st2.top()) st2.push(val);
+    }
+    
+    void pop() {
+        if(st2.top()==st1.top()) st2.pop();
+        st1.pop();
+    }
+    
+    int top() {
+        return st1.top();
+    }
+    
+    int getMin() {
+        return st2.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+int main(){
+
+}
