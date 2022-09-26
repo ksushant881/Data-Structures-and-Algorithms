d19c52a53754adeb9e076653c192e6daa8a86694
ksushant881 <ksushant881@gmail.com>
2022-09-26 23:36:33 +0530

    Satisfiability of Equality Equations

---
+++ LEETCODE/SatisfiabilityofEqualityEquations.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pi 3.1415926536
+#define ll long long int
+#define mod 1000000007
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define f(i,n) for(int i=0;i<n;i++)
+
+class Solution {
+    int uf[26];
+    int find(int x) {
+        return uf[x] == x ? x : (uf[x] = find(uf[x]));
+    }
+public:
+    bool equationsPossible(vector<string>& equations) {
+        for (int i = 0; i < 26; ++i) uf[i] = i;
+        for (auto e : equations) {
+            if (e[1] == '=') uf[find(e[0] - 'a')] = find(e[3] - 'a'); 
+        }
+        for (auto e : equations) {
+            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a')) return false;
+        }
+        return true;
+    }
+};
+
+int main(){
+
+}
