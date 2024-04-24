65cf92324b49a56e404654230d982fd5108f2a17
Sushant <71615295+ksushant881@users.noreply.github.com>
2024-04-24 08:17:09 +0530

    n-th-tribonacci-number

--- LEETCODE/n-th-tribonacci-number.cpp
+++ LEETCODE/n-th-tribonacci-number.cpp
@@ -6,7 +6,23 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define f(i,n) for(int i=0;i<n;i++)
 
-class Solution {
+class Solution1 {
+public:
+    int tribonacci(int n) {
+        if(n == 1 or n == 0) return n;
+        if(n==2) return 1;
+        int dp[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
+    }
+};
+
+class Solution2 {
 public:
 int dp[38];
   int fib1(int n){
@@ -16,7 +32,8 @@ int dp[38];
      if(dp[n]!=-1)return dp[n];
 
       return dp[n]=fib1(n-1)+fib1(n-2)+fib1(n-3);
-  }
+    }
+
     int tribonacci(int n) {
         memset(dp,-1,sizeof(dp));
         return fib1(n);
@@ -24,6 +41,5 @@ int dp[38];
 };
 
 int main(){
-    Solution s;
-    cout<<s.tribonacci(23);
+    
 }
